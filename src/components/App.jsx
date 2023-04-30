b5c14cd116fb2f45e1de7858c0e53fc8e2d43f6a
Lisiuk-Ruslan <114481433+Lysiuk-Ruslan@users.noreply.github.com>
2023-04-30 11:52:41 +0300

    Клонування ДЗ

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    ></div>
+  );
+};
